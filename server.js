844a751a5f1807b1c7572ccc440afd96d7a18e16
RidzherIT <116496741+RidzherIT@users.noreply.github.com>
2022-11-27 15:12:44 +0400

    AddServerClient

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require("path");
+const app = express()
+const port = process.env.PORT || 5000;
+
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+app.use(express.static('build', options));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')));
+
+app.listen(port, () => {
+  console.log(`React app listening at http://localhost:${port}`)
+});
